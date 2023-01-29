2033164e0cd76625d68ff4ae4be1d784119f6db0
oskar wiiala <oskar.wiiala@metropolia.fi>
2023-01-29 18:16:00 +0200

    video 2 done

--- app/src/main/java/com/example/composecourseyt/MainActivity.kt
+++ app/src/main/java/com/example/composecourseyt/MainActivity.kt
@@ -3,20 +3,47 @@ package com.example.composecourseyt
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.composecourseyt.ui.theme.ComposeCourseYTTheme
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Greeting(name = "Oskar")
+            // Prevents components from being on top of each other
+            Column {
+                Column(
+                    modifier = Modifier
+                        .height(Dp(300f)) // one way of doing Dp
+                        .width(300.dp) // another way of doing Dp
+                        .background(Color.Green),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.SpaceAround
+                ) {
+                    Text("Hello")
+                    Text("Hello2")
+                    Text("World")
+                }
+                Row(
+                    modifier = Modifier
+                        .fillMaxSize() // Fills all the space. You can also fraction fillMaxSize(0.5f)
+                        .background(Color.Green),
+                    horizontalArrangement = Arrangement.SpaceAround,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text("Hello")
+                    Text("Hello2")
+                    Text("World")
+                }
+            }
         }
     }
 }
@@ -29,5 +56,5 @@ fun Greeting(name: String) {
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-        Greeting("preview")
+    Greeting("preview")
 }

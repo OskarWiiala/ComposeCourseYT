b700b54e3bf8fd86369898527b19eb4f01b89e98
oskar wiiala <oskar.wiiala@metropolia.fi>
2023-01-30 14:04:03 +0200

    video4 done

--- app/src/main/java/com/example/composecourseyt/MainActivity.kt
+++ app/src/main/java/com/example/composecourseyt/MainActivity.kt
@@ -1,88 +1,100 @@
 package com.example.composecourseyt
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.gestures.Orientation
-import androidx.compose.foundation.gestures.draggable
-import androidx.compose.foundation.gestures.rememberDraggableState
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
-import kotlin.math.roundToInt
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Column(
+            // painterResource returns Painter, therefore the type is Painter
+            val painter = painterResource(id = R.drawable.profile_picture)
+            val description = "Profile picture"
+            val title = "This is a profile picture"
+            Box(
                 modifier = Modifier
-                    .background(Color.Green)
-                    .fillMaxHeight(0.5f)
-                    .fillMaxWidth()
-                    // modifiers are applied sequentially. Padding also stacks
-                    .border(5.dp, Color.Magenta) // border is pushed inward
-                    .padding(5.dp)
-                    .border(5.dp, Color.Blue)
-                    .padding(5.dp)
-                    .border(10.dp, Color.Red)
-                    .padding(10.dp)
-//                    .padding(top = 50.dp)
-//                    .padding(16.dp) // Will push content into container
-//                    .width(600.dp) // Will fill up to max width of screen
-//                    .requiredWidth(600.dp) // Will fill beyond max width of screen
+                    .fillMaxWidth(0.5f)
+                    .padding(16.dp)
             ) {
-                /*Text(
-                    text = "text1", modifier = Modifier
-                        // Acts much like margin, but won't push away other elements
-                        // left, bottom
-                        .offset(0.dp, 20.dp)
-                )*/
-                Text(
-                    "text1", modifier = Modifier
-                        .border(5.dp, Color.Yellow)
-                        .padding(5.dp)
-                        .offset(20.dp, 20.dp)
-                        .border(10.dp, Color.Black)
-                        .padding(10.dp)
-                )
-                Spacer(modifier = Modifier.height(50.dp))
-                Text(text = "text2", modifier = Modifier
-                    // clickable, zoomable, scrollable, draggable
-                    .clickable { Log.d("test", "test") })
-                Spacer(modifier = Modifier.height(50.dp))
-                // horizontally drag
-                var offsetX by remember { mutableStateOf(0f) }
-                Text(
-                    text = "text3", modifier = Modifier
-                        .offset { IntOffset(offsetX.roundToInt(), 0) }
-                        .draggable(
-                            orientation = Orientation.Horizontal,
-                            state = rememberDraggableState { delta ->
-                                offsetX += delta
-                            })
+                ImageCard(
+                    painter = painter,
+                    contentDescription = description,
+                    title = title
                 )
             }
+
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    Greeting("preview")
+// Composable functions ALWAYS start with a CAPITAL letter
+fun ImageCard(
+    // painter refers to the source of the image
+    painter: Painter,
+    contentDescription: String,
+    title: String,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier.fillMaxWidth(),
+        shape = RoundedCornerShape(15.dp),
+        // elevation is used for a shadow effect
+        elevation = 5.dp
+    ) {
+        // items inside box will be stacked on top of each other
+        Box(modifier = Modifier.height(175.dp)) {
+            Image(
+                painter = painter,
+                contentDescription = contentDescription,
+                // Will help scale image. Same as CenterCrop in XML.
+                contentScale = ContentScale.Crop
+            )
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    // With brush, a gradient can be applied
+                    // VerticalGradient starts from top to bottom?
+                    .background(
+                        Brush.verticalGradient(
+                            colors = listOf(Color.Transparent, Color.Black),
+                            // startY can also be calculated from the height of the box, but we use hardcoded values here
+                            startY = 300f
+                        )
+                    )
+            )
+            // New box is made to align text to left. Otherwise everything would be aligned to left.
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(12.dp),
+                // This will align the items (text) to bottom left
+                contentAlignment = Alignment.BottomStart
+            ) {
+                Text(
+                    text = title,
+                    // fontSize preferably uses sp, because it scales with user's font size preference
+                    style = TextStyle(color = Color.White, fontSize = 16.sp)
+                )
+            }
+        }
+    }
 }

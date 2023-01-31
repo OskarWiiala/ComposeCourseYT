c13f1a907435a6f5ab27298af84d30e73c2ee84c
oskar wiiala <oskar.wiiala@metropolia.fi>
2023-01-31 14:48:18 +0200

    video7 done

--- app/src/main/java/com/example/composecourseyt/MainActivity.kt
+++ app/src/main/java/com/example/composecourseyt/MainActivity.kt
@@ -7,64 +7,62 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.launch
 import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Column(modifier = Modifier.fillMaxSize()) {
-                // Three ways to save state:
-                /*val mutableState = remember { mutableStateOf(default) }
-                var value by remember { mutableStateOf(default) }
-                val (value, setValue) = remember { mutableStateOf(default) }*/
-                val color = remember {
-                    mutableStateOf(Color.Yellow)
-                }
-
-                ColorBox(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        // weight only possible in rows and columns
-                        .weight(1f)
-                ) {
-                    // This lambda block is called when ColorBox is clicked
-                    Log.d("color:", "$it")
-                    color.value = it
-                }
-                Box(
-                    modifier = Modifier
-                        .background(color.value)
-                        .weight(1f)
-                        .fillMaxSize()
-                )
+            val scaffoldState = rememberScaffoldState()
+            // with "= remember" you have to use textFieldState.value,
+            // whereas with "by remember you can directly call the variable"
+            var textFieldState by remember {
+                mutableStateOf("")
             }
+            val scope = rememberCoroutineScope()
+            // Scaffold is a layout, will make it easy to include already existing design components
+            // Such as top bars, navigation drawers and display snackbars
+            Scaffold(
+                modifier = Modifier.fillMaxSize(),
+                scaffoldState = scaffoldState
+            ) { it ->
+                Log.d("padding", "$it")
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(it)
+                        .padding(horizontal = 16.dp)
+                ) {
+                    // If you want to customize your own text field, use BasicTextField instead
+                    TextField(
+                        value = textFieldState,
+                        onValueChange = { textFieldState = it },
+                        label = { Text(text = "Your name here") },
+                        singleLine = true,
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Button(onClick = {
+                        // the snackbar thingy requires a coroutine
+                        scope.launch {
+                            // Creates snackbar
+                            scaffoldState.snackbarHostState.showSnackbar(message = "Hello $textFieldState")
+                        }
 
-
+                    }) {
+                        Text(text = "Greet me")
+                    }
+                }
+            }
         }
     }
-}
-
-@Composable
-fun ColorBox(
-    modifier: Modifier = Modifier,
-    // This is a callback function, which returns nothing, so Unit is used.
-    updateColor: (Color) -> Unit
-) {
-    Box(modifier = modifier
-        .background(Color.Red)
-        .clickable {
-            updateColor(
-                Color(
-                    Random.nextFloat(),
-                    Random.nextFloat(),
-                    Random.nextFloat(),
-                    1f
-                )
-            )
-        }
-    )
 }

e5530e43e30f60ca48d4b1a592ef70bb5dfd1727
oskar wiiala <oskar.wiiala@metropolia.fi>
2023-01-31 15:49:30 +0200

    video8 done

--- app/src/main/java/com/example/composecourseyt/MainActivity.kt
+++ app/src/main/java/com/example/composecourseyt/MainActivity.kt
@@ -7,12 +7,19 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import kotlinx.coroutines.launch
 import kotlin.random.Random
 
@@ -20,47 +27,60 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val scaffoldState = rememberScaffoldState()
-            // with "= remember" you have to use textFieldState.value,
-            // whereas with "by remember you can directly call the variable"
-            var textFieldState by remember {
-                mutableStateOf("")
-            }
-            val scope = rememberCoroutineScope()
-            // Scaffold is a layout, will make it easy to include already existing design components
-            // Such as top bars, navigation drawers and display snackbars
-            Scaffold(
-                modifier = Modifier.fillMaxSize(),
-                scaffoldState = scaffoldState
-            ) { it ->
-                Log.d("padding", "$it")
-                Column(
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.Center,
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .padding(it)
-                        .padding(horizontal = 16.dp)
-                ) {
-                    // If you want to customize your own text field, use BasicTextField instead
-                    TextField(
-                        value = textFieldState,
-                        onValueChange = { textFieldState = it },
-                        label = { Text(text = "Your name here") },
-                        singleLine = true,
-                        modifier = Modifier.fillMaxWidth()
-                    )
-                    Spacer(modifier = Modifier.height(16.dp))
-                    Button(onClick = {
-                        // the snackbar thingy requires a coroutine
-                        scope.launch {
-                            // Creates snackbar
-                            scaffoldState.snackbarHostState.showSnackbar(message = "Hello $textFieldState")
-                        }
+            // only required if using regular Column to scroll
+            // Do not use regular column as scroll unless there are only a few items
+            // val scrollState = rememberScrollState()
+            // also, if using regular column to scroll, remember to use this modifier:
+            // modifier = Modifier.verticalScroll(scrollState)
 
-                    }) {
-                        Text(text = "Greet me")
-                    }
+            // Use LazyColumn to scroll many items. It is very similar to RecyclerView in XML
+            LazyColumn {
+                itemsIndexed(
+                    listOf(
+                        "this",
+                        "is",
+                        "Jetpack",
+                        "Compose",
+                        "this",
+                        "is",
+                        "Jetpack",
+                        "Compose",
+                        "this",
+                        "is",
+                        "Jetpack",
+                        "Compose",
+                        "this",
+                        "is",
+                        "Jetpack",
+                        "Compose",
+                        "this",
+                        "is",
+                        "Jetpack",
+                        "Compose"
+                    )
+                ) { index, string ->
+                    Text(
+                        text = string,
+                        fontSize = 24.sp,
+                        fontWeight = FontWeight.Bold,
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(vertical = 24.dp)
+                    )
+                }
+                items(5000) {
+                    /*Text(
+                        // it refers to the item, which is an Int.
+                        // Might as well consider it the index
+                        text = "Item $it",
+                        fontSize = 24.sp,
+                        fontWeight = FontWeight.Bold,
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(vertical = 24.dp)
+                    )*/
                 }
             }
         }
